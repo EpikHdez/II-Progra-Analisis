f3566f4228e4b8c1f91631eeb6d88b6bf730a8aa
ErickHdez <erick.hdez06@outlook.com>
2015-11-04 08:59:58 -0600

    Cambio a sublime

--- Paralelo/mainP.c
+++ Paralelo/mainP.c
@@ -19,16 +19,16 @@ int needlemanWunsch(char h1[], char h2[])
     int F[length1][length2];
 
     for(i = 0; i < length1; i++)
-        F[i, 0] = -i;
+        F[i][0] = -i;
 
     for(j = 0; j < length2; j++)
-        F[0, j] = -j;
+        F[0][j] = -j;
 
     for(i = 1; i < length1; i++)
     {
         for(j = 1; j < length2; j++)
         {
-            match = F[i-1, j-1] + (h1[i] == h2[j] ? 1 : -1);
+            match = F[i-1][j-1] + (h1[i] == h2[j] ? 1 : -1);
             insert = F[i-1][j] - 1;
             erase = F[i][j-1] - 1;
             F[i][j] = max(match, insert, erase);
@@ -40,20 +40,20 @@ int needlemanWunsch(char h1[], char h2[])
 
 int main(int argc, char *argv[])
 {
-    MPI_Init(&argc, &argv);
-
     int rank, size;
+    char message[12];
+
+    MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     if(rank == 0)
     {
-        char message[12] = "Hola mundo!";
+        strcpy(message, "Hola mundo!");
         MPI_Send(message, sizeof(message), MPI_CHAR, 1, 0, MPI_COMM_WORLD);
     }
     if(rank == 1)
     {
-        char message[12];
         MPI_Recv(message, sizeof(message), MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         printf("Se recibio en %d el mensaje: %s desde el procesador %d.\n", rank, message, 0);
     }
